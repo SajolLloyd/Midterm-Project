2949288ef1e04f09dcee7065c3f76c76e96e8955
SajolLloyd <samesadface@gmail.com>
2020-03-15 20:52:00 +0800

    Midterm-Project

---
+++ app.js
@@ -0,0 +1,20 @@
+const navSlide = () =>  {
+    const lines = document.querySelector(".lines");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    lines.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        
+    });
+
+    navLinks.forEach((link, index) =>{
+         
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 1}s`;
+       
+
+    });
+
+}
+
+navSlide();
